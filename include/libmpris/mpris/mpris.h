00724b288d063759ce2709a226433a07d35fdd2d
mirsal <mirsal@2ccd5d93-8705-0410-8d2b-fb21a7ab8a76>
2007-07-03 13:18:13 +0000

    * Signal callbacks vtable * Corrected a typo s/MPRIS_PLAYER_LOCK/MPRIS_MPLAYER_UNLOCK/

--- include/libmpris/mpris/mpris.h
+++ include/libmpris/mpris/mpris.h
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <mpris/mpris-list.h>
 
+typedef struct _MPRISCallbackFuncs MPRISCallbackFuncs;
+
 typedef struct _MPRISPlayerInfo
 {
   struct list_head node;
@@ -15,18 +17,34 @@ typedef struct _MPRISPlayerInfo
 
 typedef struct _MPRISPlayer
 {
-  MPRISPlayerInfo * p_info;
-  pthread_mutex_t * lock;
-  pthread_t       * listen_thread;
-  int               thread_exit;
-  void            * user_data;
+  MPRISPlayerInfo       * p_info;
+  pthread_mutex_t       * lock;
+  pthread_t             * listen_thread;
+  MPRISCallbackFuncs    * callback_functions;
+  int                     thread_exit;
+  void                  * user_data;
 
 } MPRISPlayer;
+
+typedef struct _MPRISMetadata
+{
+} MPRISMetadata;
+
+typedef void (* MPRISCallbackTrackChange) (MPRISMetadata*, MPRISPlayer*, void*);
+typedef void (* MPRISCallbackStatusChange) (int, MPRISPlayer, void*);
+typedef void (* MPRISCallbackCapsChange) (int, MPRISPlayer, void*);
+
+struct _MPRISCallbackFuncs
+{
+        MPRISCallbackTrackChange  track_change;
+        MPRISCallbackStatusChange status_change;
+        MPRISCallbackCapsChange   caps_change;
+};
 
 #define MPRIS_PLAYER_NAME(player) \
         player->p_info ? (player->p_info->name) : "NOT CONNECTED"
 #define MPRIS_PLAYER_LOCK(player) pthread_mutex_lock (player->lock)
-#define MPRIS_PLAYER_LOCK(player) pthread_mutex_unlock (player->lock)
+#define MPRIS_PLAYER_UNLOCK(player) pthread_mutex_unlock (player->lock)
 
 int
 mpris_client_init   (void);
