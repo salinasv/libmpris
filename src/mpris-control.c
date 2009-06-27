f77206382325f622ce6de50ac54fd0217aa62eb5
salinasv <salinasv@gmail.com>
2009-06-27 02:19:58 -0500

    Add a simple mpris controller program. Now we can use mpris in our WM shortcuts. ;-)

---
+++ src/mpris-control.c
@@ -0,0 +1,77 @@
+#include <mpris/mpris.h>
+
+#include <getopt.h>
+#include <stdio.h>
+
+struct option long_options[] = {
+	{"prev",		0,	NULL,   'p'},
+	{"next",		0,	NULL,   'n'},
+	{"pause",   	0,	NULL,   'a'},
+	{"play",		0,	NULL,   'P'},
+	{"stop",		0,	NULL,   's'},
+	{"metadata", 	0, 	NULL, 	'm'},
+	{0,0,0,0}
+};
+
+int main(int argc, char** argv)
+{
+	MPRISPlayer *player = NULL;
+	MPRISPlayerInfo **p_list = NULL;
+	MPRISMetadata *metadata = NULL;
+	int opt;
+	int init;
+
+	init = mpris_client_init();
+
+	p_list = mpris_list_players();
+	
+	if (p_list == NULL)
+		return 1;
+
+	/* Get the first available player */
+	player = mpris_player_new(p_list[0]->suffix);
+
+	while ((opt = getopt_long(argc, argv,
+					"c:dfkmnl::v", long_options, NULL)) != -1) {
+		switch (opt) {
+			case 'p':
+				mpris_player_prev(player);
+				break;
+			case 'n':
+				mpris_player_next(player);
+				break;
+			case 'a':
+				mpris_player_pause(player);
+				break;
+			case 'P':
+				mpris_player_play(player);
+				break;
+			case 's':
+				mpris_player_stop(player);
+				break;
+			case 'm':
+				metadata = mpris_metadata_get_current_track(player);
+				printf ("Changed Track !\n"
+						"New metadata is:\n"
+						"location: %s\n"
+						"Title: %s\n"
+						"Artist: %s\n"
+						"Album: %s\n"
+						"tracknumber %s\n",
+						metadata->location,
+						metadata->title,
+						metadata->artist,
+						metadata->album,
+						metadata->tracknumber);
+				break;
+		}
+	}
+
+	if (metadata)
+		mpris_metadata_free(metadata);
+
+	mpris_player_free(player);
+
+	return 0;
+}
+
